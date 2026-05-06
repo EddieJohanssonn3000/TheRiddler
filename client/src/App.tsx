91e08b289da563b15eb61b0de301cd0719f3446e
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-05-06 10:42:37 +0200

    Changed component name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { DoorCard } from "./components/DoorCard";
 import { doors } from "./data/doors";
 import "./App.css";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 
 function App() {
   const handleDoorClick = (doorId: number) => {
