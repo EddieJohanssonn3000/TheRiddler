b65722a65a03d6fbc0d328dd3deae77354019cc1
Daniella Saadon <saadondaniella@gmail.com>
2026-05-06 11:07:24 +0200

    Create routes to root and dashboard to seperate pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Layout>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
     </Layout>
   );
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import "../App.css";
 
 import StartCodeForm from "../components/StartCodeForm";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <section>
       <h1>Step in to the Riddlers Escaperoom</h1>
       <StartCodeForm />
+      <Link to="/dashboard">Go to dashboard</Link>
     </section>
   );
 }
