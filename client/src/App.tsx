2edd46320912490f813b61b804fcdcd1c1176bec
Daniella Saadon <saadondaniella@gmail.com>
2026-05-06 10:57:12 +0200

    Clean upp App.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,11 @@
-import { DoorCard } from "./components/DoorCard";
-import { doors } from "./data/doors";
 import "./App.css";
 import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const handleDoorClick = (doorId: number) => {
-    console.log("Clicked door:", doorId);
-  };
   return (
     <Layout>
-      <h1>The Riddler</h1>
-
-      <section>
-        {doors.map((door) => (
-          <DoorCard
-            key={door.id}
-            door={door}
-            onClick={() => handleDoorClick(door.id)}
-          />
-        ))}
-      </section>
+      <HomePage />
     </Layout>
   );
 }
