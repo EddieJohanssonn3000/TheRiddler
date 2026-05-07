dcb3afee29ee532044df98d3c8289fd4322d387f
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-05-07 10:48:59 +0200

    Applied doorlock

--- client/src/data/doors.ts
+++ client/src/data/doors.ts
@@ -4,19 +4,19 @@ export const doors: Door[] = [
   {
     id: 1,
     difficulty: "easy",
-    cost: 2,
+    cost: 3,
     isUnlocked: false,
   },
   {
     id: 2,
     difficulty: "medium",
-    cost: 5,
+    cost: 2,
     isUnlocked: false,
   },
   {
     id: 3,
     difficulty: "hard",
-    cost: 10,
+    cost: 1,
     isUnlocked: false,
   },
 ];
