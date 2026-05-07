9250d4c544e974ac4e39504946de10bde272155d
Daniella Saadon <saadondaniella@gmail.com>
2026-05-07 14:50:40 +0200

    Remove button from footer, just make it a link to startpage

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="app-footer">
-      <Link to="/" className="app-btn app-btn--white">
+      <Link to="/" className="app-footer__link">
         Leave the game
       </Link>
     </footer>
