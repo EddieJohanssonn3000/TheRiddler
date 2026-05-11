8f540844c7f4ebeb7928db589140a60dd94e423d
Daniella Saadon <saadondaniella@gmail.com>
2026-05-11 09:45:58 +0200

    Normalize component filename casing: Layout.tsx

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -22,3 +22,27 @@ function Layout({ children }: Props) {
 }
 
 export default Layout;
+import { useLocation } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./Layout.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: Props) {
+  const location = useLocation();
+  const showFooter =
+    location.pathname !== "/" && location.pathname !== "/riddle";
+
+  return (
+    <div className="layout-wrapper">
+      <Header />
+      <main>{children}</main>
+      {showFooter && <Footer />}
+    </div>
+  );
+}
+
+export default Layout;
