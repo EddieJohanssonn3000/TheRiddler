df73b66bd8331bd79ff46fbc5486daf14ce5a08f
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-05-20 11:47:16 +0200

    fixed footer css

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import leaveTheGameImage from "../../assets/LeaveTheGame.png";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="app-footer">
-      <Link to="/" className="app-footer__link">
-        Leave the game
-      </Link>
+      <a
+        href="https://frontend-main-1ac7.up.railway.app/"
+        className="app-footer__link"
+        aria-label="Leave the game and return to Tivoli"
+      >
+        <img
+          src={leaveTheGameImage}
+          alt="Leave the game"
+          className="app-footer__image"
+        />
+      </a>
     </footer>
   );
 }
